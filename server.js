382942ae8a8ddef1e3c69f779df265f21dfe97b3
AdiLangs <jeremyadriancacho1@gmail.com>
2025-05-22 08:31:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ async function sendOrderConfirmationEmail(userEmail, orderDetails) {
     });
 
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: 'jeremyadriancachop@gmail.com',
         to: userEmail,
         subject: 'Order Confirmation - Lalamig',
         html: `
